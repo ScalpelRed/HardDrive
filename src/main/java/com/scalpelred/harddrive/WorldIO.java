c476945e7488142ba9c2568ed627b79a230db8cd
ScalpelRed <Ongshlingbe@yandex.ru>
2025-04-11 22:00:43 +0800

    Write function refactored

--- src/main/java/com/scalpelred/harddrive/WorldIO.java
+++ src/main/java/com/scalpelred/harddrive/WorldIO.java
@@ -8,6 +8,7 @@ import net.minecraft.world.WorldAccess;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class WorldIO {
@@ -25,22 +26,21 @@ public class WorldIO {
     public boolean WriteToWorld(WorldAccess world, BlockPos pos, File file)
             throws IOException {
 
-        try (FileInputStream f = new FileInputStream(file))
-        {
             byte[] buffer = new byte[4096];
             int bytesRead;
             int currentByteIndex;
+            long totalBytes;
 
-            int sizeX;
-            int sizeZ;
-            int sizeY;
+            int sizeX = config.sizeX.getValue();
+            int sizeZ = config.sizeZ.getValue();
+            int sizeY = config.sizeY.getValue();
 
+        try (FileInputStream f = new FileInputStream(file))
+        {
             // for some reason I like to compare these two branches to x86's protected and real modes
             // (nothing similar, actually)
-                sizeX = config.sizeX.getValue();
-                sizeZ = config.sizeZ.getValue();
-                sizeY = config.sizeY.getValue();
             if (config.appendLength.getValue()) {
+
                 long volume = (long)sizeX * sizeZ * sizeY;
                 long length = file.length();
 
@@ -58,58 +58,69 @@ public class WorldIO {
                 bytesRead = f.read(buffer, 8, buffer.length - 8);
                 if (bytesRead == -1) bytesRead = 8; // even if no data, we still have to write length
                 else bytesRead += 8;
+                totalBytes = -8;
             }
             else {
                 bytesRead = f.read(buffer, 0, buffer.length);
                 if (bytesRead == -1) return true; // no data, leaving
-
-                sizeX = config.sizeX.getValue();
-                sizeZ = config.sizeZ.getValue();
-                sizeY = config.sizeY.getValue();
+                totalBytes = 0;
             }
             currentByteIndex = 0;
-            long totalBytes = 0;
 
             BlockState block0 = Blocks.GREEN_STAINED_GLASS.getDefaultState();
             BlockState block1 = Blocks.REDSTONE_BLOCK.getDefaultState();
             int yStep = config.addLayerSpacing.getValue() ? 9 : 8;
 
             BlockPos.Mutable cpos = pos.mutableCopy();
-            for (int y = 0; y < sizeY; y++) {
-                for (int z = 0; z < sizeZ; z++) {
-                    for (int x = 0; x < sizeX; x++) {
 
-                        if (currentByteIndex == bytesRead) {
-                            bytesRead = f.read(buffer, 0, buffer.length);
-                            if (bytesRead == -1) return true;
-                            currentByteIndex = 0;
-                        }
-                        byte currentByte = buffer[currentByteIndex];
-                        currentByteIndex++;
-                        byte mask = 1;
-                        totalBytes++;
-                        if ((totalBytes & LONG_WAIT_NOTE_RATE) == 0) {
-                            hardDrive.logger.info("STILL WORKING! Wrote {} bytes so far.", totalBytes);
-                        }
+            int x = 0;
+            int z = 0;
+            int y = 0;
 
-                        for (int b = 0; b < 8; b++) {
+            while (true) {
 
-                            int bit = currentByte & mask;
-                            mask <<= 1;
-                            if (bit == 0) world.setBlockState(cpos, block0, Block.NOTIFY_ALL);
-                            else world.setBlockState(cpos, block1, Block.NOTIFY_ALL);
+                if (currentByteIndex == bytesRead) {
+                    bytesRead = f.read(buffer, 0, buffer.length);
+                    if (bytesRead == -1) return true;
+                    currentByteIndex = 0;
+                }
 
-                            cpos.move(0, 1, 0);
-                        }
-                        cpos.move(1, -8, 0);
-                    }
+                byte currentByte = buffer[currentByteIndex];
+                currentByteIndex++;
+                byte mask = 1;
+                totalBytes++;
+                if ((totalBytes & LONG_WAIT_NOTE_RATE) == 0) {
+                    hardDrive.logger.info("STILL WORKING! Wrote {} bytes so far.", totalBytes);
+                }
+
+                for (int b = 0; b < 8; b++) {
+
+                    int bit = currentByte & mask;
+                    mask <<= 1;
+                    if (bit == 0) world.setBlockState(cpos, block0, Block.NOTIFY_ALL);
+                    else world.setBlockState(cpos, block1, Block.NOTIFY_ALL);
+
+                    cpos.move(0, 1, 0);
+                }
+
+                x++;
+                cpos.move(1, -8, 0);
+                if (x >= sizeX) {
+                    x = 0;
+                    z++;
                     cpos.setX(pos.getX());
                     cpos.move(0, 0, 1);
+
+                    if (z >= sizeZ) {
+                        z = 0;
+                        y++;
+                        cpos.setZ(pos.getZ());
+                        cpos.move(0, yStep, 0);
+
+                        if (y >= sizeY) return false;
+                    }
                 }
-                cpos.setZ(pos.getZ());
-                cpos.move(0, yStep, 0);
             }
-            return false;
         }
     }
 }
