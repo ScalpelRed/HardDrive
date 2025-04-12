942bc443af848989fd30751b89144d545422a46d
ScalpelRed <Ongshlingbe@yandex.ru>
2025-04-12 14:29:30 +0800

    Safety feature at read command

--- src/main/java/com/scalpelred/harddrive/ReadFromWorldCommand.java
+++ src/main/java/com/scalpelred/harddrive/ReadFromWorldCommand.java
@@ -10,21 +10,16 @@ import net.minecraft.text.Text;
 import net.minecraft.util.math.BlockPos;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import static net.minecraft.server.command.CommandManager.argument;
 import static net.minecraft.server.command.CommandManager.literal;
 
 public class ReadFromWorldCommand {
 
-    private final int SUCCESS = 1;
-    private final int OVERWRITE_WARNING = 2;
-
-    private final int EXCEPTION_THROWN = -1;
-    private final int CEILING_HIT = -2;
-    private final int NEGATIVE_APPENDED_LENGTH = -3;
-    private final int NO_ANY_LENGTH = -4;
-
     private final HardDrive hardDrive;
+    private File outputDir;
 
     private String overwriteAskName = "";
     private boolean overwriteAsk = false;
@@ -32,6 +27,7 @@ public class ReadFromWorldCommand {
     public ReadFromWorldCommand(HardDrive hardDrive) {
 
         this.hardDrive = hardDrive;
+        outputDir = Paths.get(System.getProperty("user.dir"), "harddrive_out").toFile();
         CommandRegistrationCallback.EVENT.register(
                 (dispatcher, registryAccess, environment) -> dispatcher.register(literal("readfromworld")
                         .requires(source -> source.hasPermissionLevel(4))
@@ -53,46 +49,66 @@ public class ReadFromWorldCommand {
         ServerCommandSource src = context.getSource();
         String fileName = StringArgumentType.getString(context, "file_path");
         BlockPos pos = BlockPosArgumentType.getBlockPos(context, "position");
-        File file = new File(fileName);
-        if (file.exists()) {
-            // if we didn't ask about overwriting - we ask
-            // if we asked, but the file is different - we ask again
-            if (!overwriteAsk || !overwriteAskName.equals(fileName)) {
-                overwriteAskName = fileName;
-                overwriteAsk = true;
-                src.sendFeedback(() -> Text.literal("OVERWRITE PREVENTED: File already exists, run the command again to overwrite it."), true);
-                return OVERWRITE_WARNING;
-            }
-        }
-        overwriteAsk = false;
 
         WorldIO.IOResult res;
         try {
+            File file = new File(fileName);
+            if (!file.isAbsolute()) {
+                if (!outputDir.exists()) {
+                    outputDir = Files.createDirectories(outputDir.toPath()).toFile().getCanonicalFile();
+                }
+                file = Paths.get(outputDir.getPath(), file.getPath()).toFile();
+            }
+            file = file.getCanonicalFile();
+
+            if (!hardDrive.config.allowAnyPath.getValue()) {
+                if (!file.getPath().startsWith(outputDir.getPath())) {
+                    sendFeedback("ERROR: Access denied (use \"harddrive_out\" folder or it's subfolders)", src);
+                    return -1;
+                }
+            }
+
+            if (file.exists()) {
+                // if we didn't ask about overwriting - we ask
+                // if we asked, but the file is different - we ask again
+                if (!overwriteAsk || !overwriteAskName.equals(fileName)) {
+                    overwriteAskName = fileName;
+                    overwriteAsk = true;
+                    src.sendFeedback(() -> Text.literal("OVERWRITE PREVENTED: File already exists, run the command again to overwrite it."), true);
+                    return -1;
+                }
+            }
+            overwriteAsk = false;
+
             long length = hasImmediateLength ? LongArgumentType.getLong(context, "length") : -1;
             res = hardDrive.worldIO.readFromWorld(context.getSource().getWorld(), pos, file, length);
         } catch (Exception e) {
-            context.getSource().sendFeedback(() -> Text.literal("ERROR: Exception thrown during writing: " + e.getMessage()), true);
-            return EXCEPTION_THROWN;
+            sendFeedback("ERROR: Exception thrown during reading: " + e.getMessage(), src);
+            return -1;
         }
 
         return switch (res) {
             case SUCCESS -> {
-                src.sendFeedback(() -> Text.literal("File has been read."), true);
-                yield SUCCESS;
+                sendFeedback("File has been read.", src);
+                yield 1;
             }
             case CEILING_HIT -> {
-                src.sendFeedback(() -> Text.literal("ERROR: World upper border was reached while reading the file."), true);
-                yield CEILING_HIT;
+                sendFeedback("ERROR: World upper border was reached while reading the file.", src);
+                yield -1;
             }
             case NEGATIVE_APPENDED_LENGTH -> {
-                src.sendFeedback(() -> Text.literal("ERROR: Appended length is negative."), true);
-                yield NEGATIVE_APPENDED_LENGTH;
+                sendFeedback("ERROR: Appended length is negative.", src);
+                yield -1;
             }
             case NO_ANY_LENGTH -> {
-                src.sendFeedback(() -> Text.literal("ERROR: File length is not present neither as argument nor appended."), true);
-                yield NO_ANY_LENGTH;
+                sendFeedback("ERROR: File length is not present neither as argument nor appended.", src);
+                yield -1;
             }
             default -> 0;
         };
+    }
+
+    private void sendFeedback(String msg, ServerCommandSource src) {
+        src.sendFeedback(() -> Text.literal(msg), true);
     }
 }
